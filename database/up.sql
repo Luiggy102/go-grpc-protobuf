fddc42838a8c599a44005b7f0d3230e161518a15
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2024-11-21 16:53:32 -0500

    added db test data and test model

--- database/up.sql
+++ database/up.sql
@@ -5,3 +5,9 @@ CREATE TABLE students (
     name varchar(225) not null,
     age int not null
 );
+
+DROP TABLE IF EXISTS tests;
+CREATE TABLE tests (
+    id varchar(32) PRIMARY KEY,
+    name varchar(225) not null,
+);
